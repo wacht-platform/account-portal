d1bd1d48f66a0ec38044551ca8ba0e2c04feb114
Saurav Singh <snipextt@gmail.com>
2025-04-26 18:55:56 +0530

    wip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,21 @@
 import {
-	NavigateToSignin,
-	SignedIn,
-	SignedOut,
-	UserButton,
+  NavigateToSignIn,
+  SignedIn,
+  SignedOut,
+  UserButton,
 } from "@snipextt/wacht";
 
 export default function Home() {
-	return (
-		<>
-			<SignedIn>
-				<nav>
-					<UserButton showName={false} />
-				</nav>
-			</SignedIn>
-			<SignedOut>
-				<NavigateToSignin />
-			</SignedOut>
-		</>
-	);
+  return (
+    <>
+      <SignedIn>
+        <nav>
+          <UserButton showName={false} />
+        </nav>
+      </SignedIn>
+      <SignedOut>
+        <NavigateToSignIn />
+      </SignedOut>
+    </>
+  );
 }
