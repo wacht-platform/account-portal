bb5c4cbacea3c544e32c04112a10b692e1e368fe
Saurav Singh <snipextt@gmail.com>
2026-06-27 16:20:00 +0000

    replace /sso-callback with /social/login using SocialCallback component

--- src/app/social/login/page.tsx
+++ src/app/social/login/page.tsx
@@ -1,9 +1,9 @@
-import { SSOCallback } from "@wacht/nextjs";
+import { SocialCallback } from "@wacht/nextjs";
 
-export default function SSOCallbackPage() {
+export default function SocialLogin() {
   return (
     <div className="flex min-h-screen w-full items-center justify-center px-4 py-10">
-      <SSOCallback />
+      <SocialCallback />
     </div>
   );
 }
